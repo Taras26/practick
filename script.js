4443e3fee363b77a9195c57c67787ceb78e5d330
OrestBagan <orestbagan2233@gmail.com>
2023-04-14 16:41:31 +0300

    get value from form

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-// 'use stict'
+'use stict'
 
-// const settings = {
-//     async: true,
-//     crossDomain: true,
-//     url: 'https://send.api.mailtrap.io/api/send',
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       'Api-Token': ''
-//     },
-//     processData: false,
-//     data: '{\n  "to": [\n    {\n      "email": "john_doe@example.com",\n      "name": "John Doe"\n    }\n  ],\n  "from": {\n    "email": "sales@example.com",\n    "name": "Example Sales Team"\n  },\n "subject": "Your Example Order Confirmation",\n  "text": "Congratulations on your order no. 1234",\n  "category": "API Test"\n}'
-//   };
+const settings = {
+    async: true,
+    crossDomain: true,
+    url: 'https://send.api.mailtrap.io/api/send',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Api-Token': ''
+    },
+    processData: false,
+    data: '{\n  "to": [\n    {\n      "email": "john_doe@example.com",\n      "name": "John Doe"\n    }\n  ],\n  "from": {\n    "email": "sales@example.com",\n    "name": "Example Sales Team"\n  },\n "subject": "Your Example Order Confirmation",\n  "text": "Congratulations on your order no. 1234",\n  "category": "API Test"\n}'
+  };
   
-//   $.ajax(settings).done(function (response) {
-//     console.log(response);
-//   });
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+let obj = {}
+document.querySelector('#form').addEventListener('submit', f);
+function f(e){
+    e.preventDefault();
+    let name = $('[name=name]').val();
+    let mail = $('[name=mail]').val();
+    let text = $('[name=text]').val();
+    obj.name = name;
+    obj.mail = mail;
+    obj.text = text;
+    console.log(obj)
+}
