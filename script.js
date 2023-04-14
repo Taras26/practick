b3db719d744952284aa993a17b0108947a997d95
Taras26 <kinakh.taras@gmail.com>
2023-04-14 17:45:42 +0300

    version beta 1.1

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-'use stict'
+'use strict'
 
-const settings = {
+$( document ).ready(function() {
+  const settings = {
     async: true,
     crossDomain: true,
     url: 'https://send.api.mailtrap.io/api/send',
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Api-Token': ''
+      'Api-Token': '26eb56650c932eb07fdbae181d32fdce',
+      'Access-Control-Allow-Origin':'https://send.api.mailtrap.io/api/send'
     },
-    processData: false,
-    data: '{\n  "to": [\n    {\n      "email": "john_doe@example.com",\n      "name": "John Doe"\n    }\n  ],\n  "from": {\n    "email": "sales@example.com",\n    "name": "Example Sales Team"\n  },\n "subject": "Your Example Order Confirmation",\n  "text": "Congratulations on your order no. 1234",\n  "category": "API Test"\n}'
+    data: '{\n  "to": [\n    {\n      "email": "kinakh.taras@gmail.com",\n      "name": "Orest"\n    }\n  ],\n  "from": {\n    "email": "kinahtaras@gmail.com",\n    "name": "Taras"\n  },\n  "attachments": [\n    {\n      "content": "PCFET0NUWVBFIGh0bWw+CjxodG1sIGxhbmc9ImVuIj4KCiAgICA8aGVhZD4KICAgICAgICA8bWV0YSBjaGFyc2V0PSJVVEYtOCI+CiAgICAgICAgPG1ldGEgaHR0cC1lcXVpdj0iWC1VQS1Db21wYXRpYmxlIiBjb250ZW50PSJJRT1lZGdlIj4KICAgICAgICA8bWV0YSBuYW1lPSJ2aWV3cG9ydCIgY29udGVudD0id2lkdGg9ZGV2aWNlLXdpZHRoLCBpbml0aWFsLXNjYWxlPTEuMCI+CiAgICAgICAgPHRpdGxlPkRvY3VtZW50PC90aXRsZT4KICAgIDwvaGVhZD4KCiAgICA8Ym9keT4KCiAgICA8L2JvZHk+Cgo8L2h0bWw+Cg==",\n      "filename": "index.html",\n      "type": "text/html",\n      "disposition": "attachment"\n    }\n  ],\n  "custom_variables": {\n    "user_id": "45982",\n    "batch_id": "PSJ-12"\n  },\n  "headers": {\n    "X-Message-Source": "dev.mydomain.com"\n    \'Access-Control-Allow-Origin\':\'https://send.api.mailtrap.io/api/send\'\n  },\n  "subject": "Your Example Order Confirmation",\n  "text": "Congratulations on your order no. 1234",\n  "category": "API Test"\n}'
   };
   
   $.ajax(settings).done(function (response) {
@@ -20,7 +21,7 @@ const settings = {
 let obj = {}
 document.querySelector('#form').addEventListener('submit', f);
 function f(e){
-    e.preventDefault();
+    // e.preventDefault();
     let name = $('[name=name]').val();
     let mail = $('[name=mail]').val();
     let text = $('[name=text]').val();
@@ -29,3 +30,4 @@ function f(e){
     obj.text = text;
     console.log(obj)
 }
+});
